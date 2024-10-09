2cd74f02cacef3cbc33609543df9d8726828b6b9
richardhapb <richard.penab@gmail.com>
2024-10-09 00:38:06 -0300

    Carga dinámica CV

---
+++ src/types/CV.ts
@@ -0,0 +1,110 @@
+
+export interface CV {
+  basics: basics;
+  work: work[];
+  education: education[];
+  certificates: certificate[];
+  publications: publication[];
+  skills: skill[];
+  languages: language[];
+  interests: interest[];
+  references: reference[];
+  projects: project[];
+  softskills: string[];
+  hardskills: string[];
+}
+
+interface profile {
+  network: string;
+  username: string;
+  url: string;
+  shorturl: string;
+}
+
+interface work {
+  name: string;
+  position: string;
+  url: string;
+  startDate: string;
+  endDate: string;
+  summary: string;
+  highlights: string[];
+}
+
+interface education {
+  institution: string;
+  title: string;
+  url: string;
+  area: string;
+  studyType: string;
+  startDate: string;
+  endDate: string;
+}
+
+interface basics {
+  name: string;
+  label: string;
+  description: string;
+  image: string;
+  email: string;
+  phone: string;
+  url: string;
+  qrurl: string;
+  summary: string;
+  location: location;
+  profiles: profile[];
+}
+
+interface location {
+  address: string;
+  postalCode: string;
+  city: string;
+  countryCode: string;
+  region: string;
+}
+
+interface certificate {
+  name: string; 
+  date: string; 
+  issuer: string; 
+  url: string;
+}
+
+interface publication {
+  name: string;
+  publisher: string;
+  releaseDate: string;
+  url: string;
+  summary: string;
+}
+
+interface skill {
+  name: string;
+  level: string;
+  keywords: string[];
+}
+
+interface language {
+  language: string;
+  fluency: string;
+}
+
+interface interest {
+  name: string;
+  keywords: string[];
+}
+
+interface reference {
+  name: string;
+  reference: string;
+  phone: string;
+}
+
+interface project {
+  name: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  highlights: string[];
+  url: string;
+}
---
+++ src/utils/apicv.ts
@@ -0,0 +1,43 @@
+// apicv.ts
+
+import type { CV } from "@/types/CV"; 
+
+let dataCV: CV = {
+  basics: {name: "",
+    label: "",
+    description: "",
+    image: "",
+    email: "",
+    phone: "",
+    url: "",
+    qrurl: "",
+    summary: "",
+    location: {
+      address: "",
+      postalCode: "",
+      city: "",
+      countryCode: "",
+      region: ""},
+    profiles: []
+    },
+  work: [],
+  education: [],
+  certificates: [],
+  publications: [],
+  skills: [],
+  languages: [],
+  interests: [],
+  references: [],
+  projects: [],
+  softskills: [],
+  hardskills: []
+};
+
+export async function fetchCV() {
+  if (!dataCV.basics || Object.keys(dataCV.basics).length === 0) {
+    const response = await fetch('https://richardhapb.s3.us-east-2.amazonaws.com/resources/cv.json');
+    const data = await response.json();
+    dataCV = data
+  }
+  return dataCV;
+}
