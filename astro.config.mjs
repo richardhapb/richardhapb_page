a7a9b5acf89da7258716aede61e271855ce84d55
richardhapb <richard.penab@gmail.com>
2024-10-16 22:02:06 -0300

    cloudflare added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig, envField } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   output: 'server',
+
   env: {
     schema: {
       API_URL: {
          envField: envField.string({context: 'server', access: 'secret'})
       },
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
