fb22119cfb8e99b6ead86b406cffb57f9b994ccf
richardhapb <richard.hapb@icloud.com>
2025-02-19 22:12:35 -0300

    feat: update cv

--- src/api/cvApi.ts
+++ src/api/cvApi.ts
@@ -1,5 +1,3 @@
-// apicv.ts
-
 import type { CV } from "@/types/CV"; 
 import cv from "@cv";
 
@@ -8,7 +6,7 @@ let dataCV: CV = cv;
 // Se mantiene función para compatibilidad con api de AWS 'https://richardhapb.s3.us-east-2.amazonaws.com/resources/cv.json'
 export async function fetchCV(): Promise<CV> {
   if (!dataCV.basics || Object.keys(dataCV.basics).length === 0) {
-    const dataCV = cv;
+    dataCV = cv;
   }
   return dataCV
 }
