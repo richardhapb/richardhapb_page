be9308e8aca68271a4ed9e9e9b0336cfb79bfc69
richardhapb <richard.hapb@icloud.com>
2025-03-17 10:50:23 -0300

    feat: technologies in experience

--- src/types/CV.ts
+++ src/types/CV.ts
@@ -1,113 +1,114 @@
 
 export interface CV {
-  basics: basics;
-  work: work[];
-  education: education[];
-  certificates: certificate[];
-  publications: publication[];
-  skills: skill[];
-  languages: language[];
-  interests: interest[];
-  references: reference[];
-  projects: project[];
-  softskills: string[];
-  hardskills: string[];
+    basics: basics;
+    work: work[];
+    education: education[];
+    certificates: certificate[];
+    publications: publication[];
+    skills: skill[];
+    languages: language[];
+    interests: interest[];
+    references: reference[];
+    projects: project[];
+    softskills: string[];
+    hardskills: string[];
 }
 
 interface profile {
-  network: string;
-  username: string;
-  url: string;
-  shorturl: string;
+    network: string;
+    username: string;
+    url: string;
+    shorturl: string;
 }
 
 interface work {
-  name: string;
-  position: string;
-  url: string;
-  startDate: string;
-  endDate: string;
-  summary: string;
-  highlights: string[];
+    name: string;
+    position: string;
+    url: string;
+    startDate: string;
+    endDate: string;
+    technologies: string[],
+    summary: string;
+    highlights: string[];
 }
 
 interface education {
-  institution: string;
-  title: string;
-  url: string;
-  area: string;
-  studyType: string;
-  startDate: string;
-  endDate: string;
+    institution: string;
+    title: string;
+    url: string;
+    area: string;
+    studyType: string;
+    startDate: string;
+    endDate: string;
 }
 
 interface basics {
-  name: string;
-  label: string;
-  description: string;
-  image: string;
-  email: string;
-  phone: string;
-  url: string;
-  qrurl: string;
-  summary: string;
-  location: location;
-  profiles: profile[];
+    name: string;
+    label: string;
+    description: string;
+    image: string;
+    email: string;
+    phone: string;
+    url: string;
+    qrurl: string;
+    summary: string;
+    location: location;
+    profiles: profile[];
 }
 
 interface location {
-  address: string;
-  postalCode: string;
-  city: string;
-  countryCode: string;
-  region: string;
+    address: string;
+    postalCode: string;
+    city: string;
+    countryCode: string;
+    region: string;
 }
 
 interface certificate {
-  name: string; 
-  date: string; 
-  issuer: string; 
-  url: string;
+    name: string;
+    date: string;
+    issuer: string;
+    url: string;
 }
 
 interface publication {
-  name: string;
-  publisher: string;
-  releaseDate: string;
-  url: string;
-  summary: string;
+    name: string;
+    publisher: string;
+    releaseDate: string;
+    url: string;
+    summary: string;
 }
 
 interface skill {
-  name: string;
-  level: string;
-  keywords: string[];
+    name: string;
+    level: string;
+    keywords: string[];
 }
 
 interface language {
-  language: string;
-  fluency: string;
+    language: string;
+    fluency: string;
 }
 
 interface interest {
-  name: string;
-  keywords: string[];
+    name: string;
+    keywords: string[];
 }
 
 interface reference {
-  name: string;
-  reference: string;
-  phone: string;
+    name: string;
+    reference: string;
+    phone: string;
 }
 
 interface project {
-  name: string;
-  startDate: string;
-  endDate: string;
-  description: string;
-  image: string;
-  qrImage: string;
-  highlights: string[];
-  technologies: string[];
-  url: string;
+    name: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+    image: string;
+    qrImage: string;
+    highlights: string[];
+    technologies: string[];
+    url: string;
 }
