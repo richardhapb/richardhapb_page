63f6b47658f0030a501d59b7cb646c4d7630f6f2
richardhapb <richard.hapb@icloud.com>
2025-05-01 15:01:39 -0400

    feat: update dependencies and background image format
    - Updated "@astrojs/cloudflare" to version "^11.2.0" and moved it to
      devDependencies in package.json.
    - Upgraded "axios" to version "^1.9.0".
    - Adjusted pnpm-lock.yaml to reflect dependency changes.
    - Replaced "background-smooth-3.png" with "background-smooth-3.jpg"
      for improved performance.
    - Updated Home.astro to use the new background image format.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig, envField } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-
-  env: {
-    schema: {
-      API_URL: {
-         envField: envField.string({context: 'server', access: 'secret'})
-      },
-    }
-  },
-
-  adapter: cloudflare()
+    output: 'server',
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true,
+        },
+    }),
+    env: {
+        schema: {
+            API_URL: {
+                envField: envField.string({ context: 'server', access: 'secret' })
+            },
+        }
+    },
 });
